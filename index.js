22ad10d280596d0e4bf6f5998b094d1c7afdadf2
OwethuSotomela <owethusotomela@gmail.com>
2021-11-05 09:43:42 +0200

    index setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const basket = require('./fruitBasket');
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/fruitbasket';
+
+const pool = new Pool({
+	connectionString: connectionString,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+const PORT = process.env.PORT || 1919;
+app.listen(PORT, function(){
+    console.log("App started at port", PORT);
+});
