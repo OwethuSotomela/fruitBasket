355a625518c8221f4d12a4d9d284e8d8de740aa2
OwethuSotomela <owethusotomela@gmail.com>
2021-11-11 12:59:53 +0200

    Travis

--- test/fruit-basket-test.js
+++ test/fruit-basket-test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const tastyFruit = require("../fruitBasket");
 const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/fruitBasket';
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/travis_ci_test';
 
 const pool = new Pool({
     connectionString: connectionString,
@@ -72,6 +72,7 @@ describe('Show Total Price', async function () {
         getFruitName.forEach(element => {
             fruitBasket.push(element.price)
         });
+        console.log(fruitBasket)
 
         assert.equal(bananaBasket.length, fruitBasket.length, await basket.showPrice());
     })
